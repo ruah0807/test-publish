aa784286d4a6c1331d538366201ae895ca89a74a
ruah0807 <kimhk0315@gmail.com>
2025-06-17 15:07:16 +0900

    패키지 의존성에 'vite-plugin-svgr' 추가 및 SVG 아이콘 파일로 교체. 기존 PNG 아이콘 파일 삭제, 사이드바 컴포넌트에서 SVG 아이콘 사용으로 변경.

--- src/components/3.templates/Sidebar.tsx
+++ src/components/3.templates/Sidebar.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import styles from "./Sidebar.module.scss";
-import dashboardIcon from "../../../public/images/dashboard-icon.png";
-import chatIcon from "../../../public/images/chatbot.png";
+import dashboardIcon from "../../../public/images/dashboard.svg";
+import dashboardIconActive from "../../../public/images/dashboard_active.svg";
+import chatIcon from "../../../public/images/chatbot.svg";
+import chatIconActive from "../../../public/images/chatbot_active.svg";
 import educationIcon from "../../../public/images/education.svg";
+import educationIconActive from "../../../public/images/education_active.svg";
 import hamburgerIcon from "../../../public/images/hamburger.png";
+
 const Sidebar = () => {
+  const [active, setActive] = useState(0);
+
   return (
     <>
       <div className={styles.menuName}>
@@ -16,16 +23,25 @@ const Sidebar = () => {
       </div>
       <nav>
         <ul className={styles.menuContainer}>
-          <li className={styles.menuItem}>
-            <img src={dashboardIcon} alt="dashboardIcon" />
+          <li onClick={() => setActive(0)} className={styles.menuItem}>
+            <img
+              src={active === 0 ? dashboardIconActive : dashboardIcon}
+              alt="dashboardIcon"
+            />
             <div className={styles.menuText}>대시보드</div>
           </li>
-          <li className={styles.menuItem}>
-            <img src={chatIcon} alt="chatIcon" />
+          <li className={styles.menuItem} onClick={() => setActive(1)}>
+            <img
+              src={active === 1 ? chatIconActive : chatIcon}
+              alt="chatIcon"
+            />
             <div className={styles.menuText}>AI Q&A Chat</div>
           </li>
-          <li className={styles.menuItem}>
-            <img src={educationIcon} alt="educationIcon" />
+          <li className={styles.menuItem} onClick={() => setActive(2)}>
+            <img
+              src={active === 2 ? educationIconActive : educationIcon}
+              alt="educationIcon"
+            />
             <div className={styles.menuText}>교육</div>
           </li>
         </ul>
